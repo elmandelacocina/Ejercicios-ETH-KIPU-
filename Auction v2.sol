6a254f444e6b26a37bed767647046cae29f6ff04
elmandelacocina <72666498+elmandelacocina@users.noreply.github.com>
2025-06-11 23:10:31 -0300

    Fix typos, import guard, update README, add test

--- Auction v2.sol
+++ Auction v2.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+
 
 /**
  * @title AuctionTP - Full-featured ETH auction with extensions and safe withdrawal
